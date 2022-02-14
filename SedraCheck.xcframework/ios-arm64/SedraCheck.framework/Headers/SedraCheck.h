681eaf8783fcd21c9d9b066d336ef184b97fdcfc
Zaid Abbas <mob@sedrapay.com>
2022-02-14 18:01:56 +0200

    sdk

---
+++ SedraCheck.xcframework/ios-arm64/SedraCheck.framework/Headers/SedraCheck.h
@@ -0,0 +1,18 @@
+//
+//  SedraCheck.h
+//  SedraCheck
+//
+//  Created by Zaid Abbas BTIT on 14/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SedraCheck.
+FOUNDATION_EXPORT double SedraCheckVersionNumber;
+
+//! Project version string for SedraCheck.
+FOUNDATION_EXPORT const unsigned char SedraCheckVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SedraCheck/PublicHeader.h>
+
+#import <SedraCheck/SedraCheck.h>
