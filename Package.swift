15d348037fc0c804539a91cccc89c3283ba5a659
zabbasSedraPay <156678605+zabbasSedraPay@users.noreply.github.com>
2024-01-16 17:31:35 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,18 +1,25 @@
 // swift-tools-version:5.7
 import PackageDescription
 
+import PackageDescription
+
 let package = Package(
-    name: "YourFrameworkName",
+    name: "SedraCheck",
     platforms: [
         .iOS(.v11)
     ],
     products: [
-        .library(name: "SedraCheck", targets: ["SedraCheck"])
+        .library(
+            name: "SedraCheck",
+            targets: ["SedraCheck"]
+        ),
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "SedraCheck",
-            path: "SedraCheck.xcframework"
-        )
+            url: "https://github.com/SedraPay/SedraCheck/releases/download/1.6.1/SedraCheck.xcframework.zip",
+            checksum: "403a42a5ab30861d79761ba229d33b91e74b31d810844effc129f774a5ffddea")
+        ),
     ]
 )
+
