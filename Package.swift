3eec587ea7fcee3d49a271292424e523c6c2e558
zabbasSedraPay <156678605+zabbasSedraPay@users.noreply.github.com>
2024-01-16 17:00:50 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SedraCheck",
-            url: "https://github.com/SedraPay/SedraCheck/blob/main/SedraCheck.xcframework.zip",
+            url: "https://github.com/SedraPay/SedraCheck/archive/refs/tags/1.6.1.zip",
             checksum: "403a42a5ab30861d79761ba229d33b91e74b31d810844effc129f774a5ffddea")
         ),
     ]
