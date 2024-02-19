e813f6fbd7bd28d67b16611d89e6e2de1d5e9ff5
zabbasSedraPay <156678605+zabbasSedraPay@users.noreply.github.com>
2024-02-19 14:28:55 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version:5.7
-import PackageDescription
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SedraCheck",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v12)
     ],
     products: [
         .library(
@@ -14,12 +14,13 @@ let package = Package(
             targets: ["SedraCheck"]
         ),
     ],
+    dependencies: [
+    ],
     targets: [
         .binaryTarget(
             name: "SedraCheck",
-            url: "https://github.com/SedraPay/SedraCheck/releases/download/1.6.1/SedraCheck.xcframework.zip",
-            checksum: "403a42a5ab30861d79761ba229d33b91e74b31d810844effc129f774a5ffddea")
+            url: "https://github.com/SedraPay/SedraCheck/releases/download/1.6.2/SedraCheck.xcframework.zip",
+            checksum: "403a42a5ab30861d79761ba229d33b91e74b31d810844effc129f774a5ffddea"
         ),
     ]
 )
-
